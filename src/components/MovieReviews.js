0383f575a0a00e0dc91ee4fedab5b70730604ad7
Andrew Waller <andrewhwaller@gmail.com>
2019-09-27 22:52:43 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,11 +1,11 @@
 // Code MovieReviews Here
 import React, { Component } from 'react';
 
-class MovieReviews =
+class MovieReviews extends Component {
   constructor = (props) => {
     <div className={review-list}>
     </div>
-};
+  };
 
-
+}
 export default MovieReviews;
